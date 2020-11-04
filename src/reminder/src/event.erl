98f99bf287efc36bc81926245309cbea1bc119c4
Vy Hong <vhong@amazon.com>
2020-11-03 22:26:12 -0800

    Add basic event.erl

---
+++ src/reminder/src/event.erl
@@ -0,0 +1,19 @@
+-module(event).
+-compile(export_all).
+-record(state, {server, name="", to_go=0}).
+
+loop(S = #state{server=Server, to_go=[T|Next]}) ->
+    receive
+        {Server, Ref, cancel} ->
+            Server ! {Ref, ok}
+        after T * 1000 ->
+            if Next =:= [] ->
+                Server ! {done, S#state.name};
+               Next =/= [] ->
+                loop(S#state{to_go=Next})
+            end
+    end.
+
+normalize(N) ->
+    Limit = 49 * 24 * 60 * 60,
+    [N rem Limit | lists:duplicate(N div Limit, Limit)].
